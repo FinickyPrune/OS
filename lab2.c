e82de5bbd28c4dfb0fb4ae610390538a87ea6bd2
Anastasia Kravchenko <a.kravchenko2@g.nsu.ru>
2021-02-11 23:45:54 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -8,18 +8,24 @@
  main()
  {
     time_t now;
-    struct tm *sp;
+    struct tm *struct_pointer;
 
     putenv ("TZ=America/Los_Angeles");
+    
+    // функция  int putenv(char*) кладет указатель на строку в extern char** environ
+    // проверить, какие часовые пояса поддерживаются ОС можно в каталоге /usr/share/lib/zoneinfo/ 
+
     (void) time( &now );
+
+    // time_t time(time_t* tloc) возвращает значение времени и если tloc не null записывает по нему возвращаемое значение 
 
     printf("%s", ctime( &now ) );
 
-    sp = localtime(&now);
+    struct_pointer = localtime(&now);
     
     printf("%d/%d/%02d %d:%02d %s\n",
-        sp->tm_mon + 1, sp->tm_mday,
-        sp->tm_year, sp->tm_hour,
-        sp->tm_min, tzname[sp->tm_isdst]);
+        struct_pointer->tm_mon + 1, struct_pointer->tm_mday,
+        struct_pointer->tm_year, struct_pointer->tm_hour,
+        struct_pointer->tm_min, tzname[struct_pointer->tm_isdst]);
     exit(0);
  }
