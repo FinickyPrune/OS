8990b33c94b3ca2c4874e7f0dea84bc1ef68ec4f
Anastasia Kravchenko <a.kravchenko2@g.nsu.ru>
2021-02-12 00:22:16 +0700

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -21,8 +21,15 @@
 
     printf("%s", ctime( &now ) );
 
+    // char* ctime(time_t* clock) конвертирует clock в строку определенного формата и возвращает указатель на нее 
+
     struct_pointer = localtime(&now);
     
+    // struct tm* localtime(time_t clock) 
+    // 1) вызывает void tzset(void), которая в свою очередь по информации в TZ переписывает внешние перерменные
+    // 2) также tzset инициализирует tzname[]
+    // 3) конвертирует clock в поля структуры tm и возвращает на нее указатель
+
     printf("%d/%d/%02d %d:%02d %s\n",
         struct_pointer->tm_mon + 1, struct_pointer->tm_mday,
         struct_pointer->tm_year, struct_pointer->tm_hour,
