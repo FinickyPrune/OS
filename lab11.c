63176591bf18f238437248e8440ee7a3dd26142a
Anastasia Kravchenko <a.kravchenko2@g.nsu.ru>
2021-04-23 08:53:37 +0700

    Update lab11.c

--- lab11.c
+++ lab11.c
@@ -5,6 +5,7 @@
 #define INIT_CHECK 0
 #define PUTENV_ERROR 1
 #define EXEC_ERROR -1
+#define END_OF_ENVP NULL
 
 extern char** environ;
 
@@ -31,16 +32,21 @@ int main(int argc, char* argv[], char* envp[]){
         return EXIT_FAILURE;
     }
   
-    int putenv_check = INIT_CHECK;
-    putenv_check = putenv("TZ=America/Los_Angeles");
-    if (putenv_check >= PUTENV_ERROR)
-    {
-        perror("Putenv executed with error");
-        return EXIT_FAILURE;
-    }  
+    // int putenv_check = INIT_CHECK;
+    // putenv_check = putenv("TZ=America/Los_Angeles");
+    // if (putenv_check >= PUTENV_ERROR)
+    // {
+    //     perror("Putenv executed with error");
+    //     return EXIT_FAILURE;
+    // }  
+
+    //char* spoiled_envp[2] = {"PATH=/home/students/19200/a.kravchenko2/lab11/spoiled_cat", END_OF_ENVP};
+    char* spoiled_envp[2] = {"PATH=/home/students/19200/a.kravchenko2/lab11/dog", END_OF_ENVP};
 
     int execvpe_check = INIT_CHECK;
-    execvpe_check = execvpe(argv[1], &argv[1], envp);
+    //execvpe_check = execvpe(argv[1], &argv[1], envp);
+    execvpe_check = execvpe(argv[1], &argv[1], spoiled_envp);
+    
     if (execvpe_check == EXEC_ERROR)
     {
         perror("Execvpe executed with error");
